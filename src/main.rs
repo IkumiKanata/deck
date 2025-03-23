3d62a4b83f29ccf2535ca0b9c2553c5774c0a829
KanataIkumi <i.kanata0531@gmail.com>
2025-03-23 10:23:56 +0900

    依存関係にrandを追加し、Deck構造体にシャッフル機能を実装

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,41 @@
+use rand::{rngs::ThreadRng, seq::SliceRandom};
+
 #[derive(Debug)]
 
 struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
-    let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+impl Deck {
+    fn new() -> Deck {
+        let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+        let values = [
+            "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace",
+        ];
 
-    let mut cards = vec![];
+        let mut cards = vec![];
 
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+
+        Deck { cards }
     }
 
-    let deck = Deck { cards};
+    fn shuffle(&mut self) {
+        let mut rng = ThreadRng::default();
+        let mut cards = self.cards.clone();
+        cards.shuffle(&mut rng);
+        self.cards = cards;
+    }
+}
+
+fn main() {
+    let mut deck = Deck::new();
+    deck.shuffle();
 
     println!("Here is your deck: {:#?}", deck);
 }
